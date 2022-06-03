15f1ef37d5f918321949c1da0e7bb9e4486aa64c
ShuaiShuai <1223086236@qq.com>
2022-06-03 15:56:59 +0800

    更新

---
+++ kafka-learing/src/main/java/com/alibaba/kafkalearing/KafkaLearingApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.kafkalearing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLearingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaLearingApplication.class, args);
+    }
+
+}
